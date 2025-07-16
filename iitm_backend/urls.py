310641f97140a8e9e255885aab2e53227cfc97cd
nagipragalathan <nagipragalathan@gmail.com>
2025-07-16 14:35:28 +0530

    Update Vercel configuration: Changed distribution directory from 'staticfiles_build' to 'staticfiles' for deployment.

--- iitm_backend/urls.py
+++ iitm_backend/urls.py
@@ -45,7 +45,8 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-    
+
 # Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
