55de57268c9720617e11e0c4515f06f572b5a352
nagipragalathan <nagipragalathan@gmail.com>
2025-07-16 14:40:37 +0530

    Update ALLOWED_HOSTS in settings.py: Changed from an empty list to allow all hosts for development purposes.

--- iitm_backend/settings.py
+++ iitm_backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-%3lcg(d$@-8+w4i*zf-x*q(z%#s23gur_*thfvmxcvjucl3rqq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
