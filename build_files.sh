cb543b60ceb389f6b0cfe57cee03c6320aef41ff
nagipragalathan <nagipragalathan@gmail.com>
2025-07-18 12:23:36 +0530

    Enhance build_files.sh: Implement dynamic Python version detection for dependency installation and streamline static file collection process, ensuring compatibility with multiple Python versions.

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,22 @@
-#!/bin/bash
+# Check the installed Python version and adjust the command accordingly
+PYTHON_VERSION=$(python3 --version)
+if [[ $PYTHON_VERSION == *"3.12"* ]]; then
+    PYTHON_COMMAND="python3.12"
+elif [[ $PYTHON_VERSION == *"3.11"* ]]; then
+    PYTHON_COMMAND="python3.11"
+elif [[ $PYTHON_VERSION == *"3.9"* ]]; then
+    PYTHON_COMMAND="python3.9"
+else
+    PYTHON_COMMAND="python3"  # Fallback to default Python 3 version
+fi
 
-echo "Installing dependencies..."
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
-python3.12 -m pip install whitenoise --upgrade
+  
+# Replace `python3.11.5` with `$PYTHON_COMMAND`
+$PYTHON_COMMAND manage.py collectstatic --noinput
 
-echo "Creating static directory..."
-mkdir -p staticfiles
+$PYTHON_COMMAND -m pip install -r requirements.txt
+# Create the output directory if it doesn't exist
+mkdir -p staticfiles_build
 
-echo "Collecting static files..."
-python3.12 manage.py collectstatic --noinput --clear
-
-echo "Build completed!"
+# Move the static files into the output directory
+mv media staticfiles_build
